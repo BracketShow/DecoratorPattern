9754c37702a8829d538ff5b619071d5951dfa2e6
Bruno Barrette <bruno.barrette@gmail.com>
2016-12-02 07:52:07 -0500

    Added VR decorator

--- DecoratorExampleFinal/MultiplayerGameDecorator.cs
+++ DecoratorExampleFinal/MultiplayerGameDecorator.cs
@@ -18,4 +18,16 @@ namespace DecoratorExampleFinal
         public override string Description => base.Description + $" Avec possibilité de jouer jusqu'à {NumberOfPlayers} joueurs!";
     }
 
+    public class VRGameDecorator : VideoGameDecorator
+    {
+        public VRGameDecorator(VideoGame game, int numberOfPlayers) : base(game)
+        {
+            NumberOfPlayers = numberOfPlayers;
+        }
+
+        public override int NumberOfPlayers { get; }
+
+        public override string Description => "Mode VR disponible !!! " + base.Description;
+    }
+
 }
